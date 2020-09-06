b4d2227e1aae8ac062c9aa79cbe9df9601959aec
Nick9499 <45527115+Nick9499@users.noreply.github.com>
2020-09-06 22:33:46 +0530

    Add files via upload

---
+++ build/precache-manifest.b25c45021147d5b4de70d63a9a259c97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "754595ee42c868891119c602cca3550f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e84b73bcbb8f52c6555",
+    "url": "/static/css/main.d14e2b3f.chunk.css"
+  },
+  {
+    "revision": "a9acaec0fc641f8559fd",
+    "url": "/static/js/2.96f16297.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.96f16297.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e84b73bcbb8f52c6555",
+    "url": "/static/js/main.f986c52e.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);
